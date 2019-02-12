0244dc3f6d957d5366ff859af0c1e5e0307c940b
Crexfu <chrisdirks1@gmail.com>
2019-02-12 13:57:13 -0600

    email testing

--- server.js
+++ server.js
@@ -1,8 +1,47 @@
-var express = require('express')
+const express = require('express');
 const path = require('path');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const app = express();
 
-var app = express()
+require('dotenv').config();
 
+app.options('*', cors()) // include before other routes
 app.use(express.static(path.join(__dirname, '../chris-website/build')));
+app.use(bodyParser.json())
+app.listen(3001)
 
-app.listen(3000)
+app.post('/mail', async (req, res) =>{
+
+    let nodemailer = require('nodemailer');
+
+    console.log(process.env.MAIL_SERVER);
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_SERVER,
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      name: process.env.MAIL_SERVER,
+      auth: {
+        user: process.env.MAIL_USER, // generated ethereal user
+        pass: process.env.MAIL_PASS // generated ethereal password
+      }
+    });
+
+    let mailOptions = { 
+      from: req.body.name + ' ' + req.body.email, // sender address
+      to: "chrisdirks.developer@gmail.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: req.body.message, // plain text body
+      html: "" // html body
+    };
+
+    let info = await transporter.sendMail(mailOptions)
+
+    console.log("Message sent: %s", info.messageId);
+
+    res.status(200);
+})
+
+app.get('/', async (req, res) =>{
+    res.status(200);
+})
